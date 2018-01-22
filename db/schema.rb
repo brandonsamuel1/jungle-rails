21f8df87990f3295f60f1d3d320ca6a7c984cda5
Brandon Samuel <brandonsamuel72@gmail.com>
2018-01-22 20:16:43 +0000

    completed the practice tests for jungle

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180119202908) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
 
   create_table "users", force: :cascade do |t|
